d33c34325f8a8c00d88fdebfc3b44b7b46f2f006
ZigaoWang <zigao.wang24@gmail.com>
2023-10-11 07:37:49 +0800

    Update v0.2.10

--- script.js
+++ script.js
@@ -370,3 +370,31 @@ var myDate = new Date();
 
     document.getElementById('greet').innerHTML =
         '<div>' + greet + '</div>';
+
+function updateOnlineTime() {
+  // Set the start date and time (August 17, 2023, 7:32 PM)
+  const startDate = new Date("2023-08-17T19:32:29");
+  const currentDate = new Date();
+  const timeDifference = currentDate - startDate;
+  
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  const daysElement = document.getElementById("days");
+  const hoursElement = document.getElementById("hours");
+  const minutesElement = document.getElementById("minutes");
+  const secondsElement = document.getElementById("seconds");
+
+  daysElement.textContent = days;
+  hoursElement.textContent = hours;
+  minutesElement.textContent = minutes;
+  secondsElement.textContent = seconds;
+}
+
+// Call the updateOnlineTime function initially
+updateOnlineTime();
+
+// Update the time every second
+setInterval(updateOnlineTime, 1000);
